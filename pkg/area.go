546b543e09ef20f283e978422d2145249787045d
Yuuki Takahashi <20282867+yktakaha4@users.noreply.github.com>
2022-12-17 18:56:29 +0900

    add kunitori

--- pkg/area.go
+++ pkg/area.go
@@ -1,12 +1,117 @@
 package pkg
 
 import (
+	"errors"
 	"fmt"
+	"log"
+	"math"
+	"sort"
 )
+
+type AreaAuthor struct {
+	Area      Area
+	AreaRatio float64
+	Author    string
+}
+
+func Kunitori(areaInfo *AreaInfo, result *CountLinesResult) ([]*AreaAuthor, error) {
+	type rank struct {
+		author     string
+		lines      int
+		linesRatio float64
+		areaRatio  float64
+	}
+
+	log.Printf("start GetAreaAuthors: areaInfo=%+v, result=%+v", areaInfo, result)
+
+	if len(result.LinesByAuthor) == 0 {
+		return nil, errors.New("CountLinesResult is empty")
+	}
+
+	totalAuthors, totalLines := 0, 0
+	for _, lines := range result.LinesByAuthor {
+		totalLines += lines
+		totalAuthors++
+	}
+
+	log.Printf("count: totalAuthors=%v, totalLines=%v", totalAuthors, totalLines)
+
+	ranks := make([]rank, 0)
+	for author, lines := range result.LinesByAuthor {
+		ranks = append(ranks, rank{
+			author:     author,
+			lines:      lines,
+			linesRatio: float64(lines) / float64(totalLines),
+			areaRatio:  0,
+		})
+	}
+
+	sort.SliceStable(ranks, func(i, j int) bool {
+		if ranks[i].lines == ranks[j].lines {
+			return ranks[i].author < ranks[j].author
+		} else {
+			return ranks[i].lines > ranks[j].lines
+		}
+	})
+
+	log.Printf("ranks: count=%+v", len(ranks))
+
+	totalAreaSize := float64(0)
+	for _, area := range areaInfo.Areas {
+		totalAreaSize += area.Size
+	}
+
+	log.Printf("count: areaCount=%v totalAreaSize=%v", len(areaInfo.Areas), totalAreaSize)
+
+	log.Printf("start kunitori!")
+
+	areaAuthors := make([]*AreaAuthor, 0)
+	fraction := float64(1)
+	for _, area := range areaInfo.Areas {
+		areaRatio := area.Size / totalAreaSize
+		author := ""
+		for index, rank := range ranks {
+			if rank.linesRatio >= rank.areaRatio+areaRatio {
+				log.Printf(
+					"allocate: area=%v, areaRatio=%.2f => author=%v, linesRatio=%.2f, authorAreaRatio=%.2f => %.2f",
+					area.Name,
+					areaRatio,
+					rank.author,
+					rank.linesRatio,
+					rank.areaRatio,
+					rank.areaRatio+areaRatio,
+				)
+
+				author = rank.author
+				ranks[index].areaRatio += areaRatio
+				break
+			}
+		}
+
+		if author == "" {
+			log.Printf("skip: area=%v, areaRatio=%v", area.Name, areaInfo)
+		}
+
+		roundedAreaRatio := math.Round(areaRatio*1000) / 1000
+		areaAuthors = append(areaAuthors, &AreaAuthor{
+			Area:      area,
+			Author:    author,
+			AreaRatio: roundedAreaRatio,
+		})
+		fraction -= roundedAreaRatio
+	}
+
+	roundedFraction := math.Round(fraction*1000) / 1000
+	areaAuthors[len(areaAuthors)-1].AreaRatio += roundedFraction
+
+	log.Printf("complete kunitori: areaAuthors=%v, roundedFraction=%v", len(areaAuthors), roundedFraction)
+
+	return areaAuthors, nil
+}
 
 type Area struct {
 	Name string
-	Size int
+	Size float64
 }
 
 type AreaInfo struct {
@@ -20,53 +125,53 @@ func GetAreaInfo(region string) (*AreaInfo, error) {
 		return &AreaInfo{
 			Region: region,
 			Areas: []Area{
-				{Name: "北海道", Size: 83424},
-				{Name: "青森", Size: 9646},
-				{Name: "岩手", Size: 15275},
-				{Name: "宮城", Size: 7282},
-				{Name: "秋田", Size: 11638},
-				{Name: "山形", Size: 9323},
-				{Name: "福島", Size: 13784},
-				{Name: "茨城", Size: 6097},
-				{Name: "栃木", Size: 6408},
-				{Name: "群馬", Size: 6362},
-				{Name: "埼玉", Size: 3798},
-				{Name: "千葉", Size: 5158},
-				{Name: "東京", Size: 2191},
-				{Name: "神奈川", Size: 2416},
-				{Name: "新潟", Size: 12584},
-				{Name: "富山", Size: 4248},
-				{Name: "石川", Size: 4186},
-				{Name: "福井", Size: 4190},
-				{Name: "山梨", Size: 4465},
-				{Name: "長野", Size: 13562},
-				{Name: "岐阜", Size: 10621},
-				{Name: "静岡", Size: 7777},
-				{Name: "愛知", Size: 5172},
-				{Name: "三重", Size: 5774},
-				{Name: "滋賀", Size: 4017},
-				{Name: "京都", Size: 4612},
-				{Name: "大阪", Size: 1905},
-				{Name: "兵庫", Size: 8401},
-				{Name: "奈良", Size: 3691},
-				{Name: "和歌山", Size: 4725},
-				{Name: "鳥取", Size: 3507},
-				{Name: "島根", Size: 6708},
-				{Name: "岡山", Size: 7115},
-				{Name: "広島", Size: 8479},
-				{Name: "山口", Size: 6112},
-				{Name: "徳島", Size: 4147},
-				{Name: "香川", Size: 1877},
-				{Name: "愛媛", Size: 5676},
-				{Name: "高知", Size: 7104},
-				{Name: "福岡", Size: 4986},
-				{Name: "佐賀", Size: 2441},
-				{Name: "長崎", Size: 4132},
-				{Name: "熊本", Size: 7409},
-				{Name: "大分", Size: 6341},
-				{Name: "宮崎", Size: 7735},
-				{Name: "鹿児島", Size: 9187},
-				{Name: "沖縄", Size: 2281},
+				{Name: "Hokkaido", Size: 83424},
+				{Name: "Aomori", Size: 9646},
+				{Name: "Iwate", Size: 15275},
+				{Name: "Miyagi", Size: 7282},
+				{Name: "Akita", Size: 11638},
+				{Name: "Yamagata", Size: 9323},
+				{Name: "Fukushima", Size: 13784},
+				{Name: "Ibaraki", Size: 6097},
+				{Name: "Tochigi", Size: 6408},
+				{Name: "Gunma", Size: 6362},
+				{Name: "Saitama", Size: 3798},
+				{Name: "Chiba", Size: 5158},
+				{Name: "Tokyo", Size: 2191},
+				{Name: "Kanagawa", Size: 2416},
+				{Name: "Niigata", Size: 12584},
+				{Name: "Toyama", Size: 4248},
+				{Name: "Ishikawa", Size: 4186},
+				{Name: "Fukui", Size: 4190},
+				{Name: "Yamanashi", Size: 4465},
+				{Name: "Nagano", Size: 13562},
+				{Name: "Gifu", Size: 10621},
+				{Name: "Shizuoka", Size: 7777},
+				{Name: "Aichi", Size: 5172},
+				{Name: "Mie", Size: 5774},
+				{Name: "Shiga", Size: 4017},
+				{Name: "Kyoto", Size: 4612},
+				{Name: "Osaka", Size: 1905},
+				{Name: "Hyogo", Size: 8401},
+				{Name: "Nara", Size: 3691},
+				{Name: "Wakayama", Size: 4725},
+				{Name: "Tottori", Size: 3507},
+				{Name: "Shimane", Size: 6708},
+				{Name: "Okayama", Size: 7115},
+				{Name: "Hiroshima", Size: 8479},
+				{Name: "Yamaguchi", Size: 6112},
+				{Name: "Tokushima", Size: 4147},
+				{Name: "Kagawa", Size: 1877},
+				{Name: "Ehime", Size: 5676},
+				{Name: "Kouchi", Size: 7104},
+				{Name: "Fukuoka", Size: 4986},
+				{Name: "Saga", Size: 2441},
+				{Name: "Nagasaki", Size: 4132},
+				{Name: "Kumamoto", Size: 7409},
+				{Name: "Oita", Size: 6341},
+				{Name: "Miyazaki", Size: 7735},
+				{Name: "Kagoshima", Size: 9187},
+				{Name: "Okinawa", Size: 2281},
 			},
 		}, nil
 	}
--- pkg/area_test.go
+++ pkg/area_test.go
@@ -1,9 +1,102 @@
 package pkg
 
 import (
+	"fmt"
 	"github.com/stretchr/testify/assert"
 	"testing"
 )
+
+func TestKunitori(t *testing.T) {
+	areaInfo := AreaInfo{
+		Region: "TestArea",
+		Areas: []Area{
+			{Name: "Area100", Size: 100},
+			{Name: "Area75", Size: 75},
+			{Name: "Area50", Size: 50},
+			{Name: "Area25", Size: 25},
+			{Name: "Area20", Size: 20},
+			{Name: "Area15", Size: 15},
+			{Name: "Area10", Size: 10},
+			{Name: "Area5", Size: 5},
+		},
+	}
+
+	result := CountLinesResult{
+		LinesByAuthor: map[string]int{
+			"userA": 300,
+			"userB": 200,
+			"userC": 100,
+		},
+	}
+
+	testCases := []struct {
+		areaInfo    *AreaInfo
+		result      *CountLinesResult
+		areaAuthors []*AreaAuthor
+	}{
+		{
+			areaInfo: &areaInfo,
+			result:   &result,
+			areaAuthors: []*AreaAuthor{
+				{
+					Area:      areaInfo.Areas[0],
+					AreaRatio: 0.333,
+					Author:    "userA",
+				},
+				{
+					Area:      areaInfo.Areas[1],
+					AreaRatio: 0.25,
+					Author:    "userB",
+				},
+				{
+					Area:      areaInfo.Areas[2],
+					AreaRatio: 0.167,
+					Author:    "userA",
+				},
+				{
+					Area:      areaInfo.Areas[3],
+					AreaRatio: 0.083,
+					Author:    "userB",
+				},
+				{
+					Area:      areaInfo.Areas[4],
+					AreaRatio: 0.067,
+					Author:    "userC",
+				},
+				{
+					Area:      areaInfo.Areas[5],
+					AreaRatio: 0.05,
+					Author:    "userC",
+				},
+				{
+					Area:      areaInfo.Areas[6],
+					AreaRatio: 0.033,
+					Author:    "userC",
+				},
+				{
+					Area:      areaInfo.Areas[7],
+					AreaRatio: 0.017,
+					Author:    "userC",
+				},
+			},
+		},
+	}
+
+	for index, testCase := range testCases {
+		t.Run(fmt.Sprintf("case_%v", index), func(t *testing.T) {
+			areaAuthors, err := Kunitori(testCase.areaInfo, testCase.result)
+			assert.NoError(t, err)
+			assert.Equal(t, len(testCase.areaInfo.Areas), len(areaAuthors))
+			assert.Equal(t, testCase.areaAuthors, areaAuthors)
+
+			totalRatio := float64(0)
+			for _, areaAuthor := range areaAuthors {
+				totalRatio += areaAuthor.AreaRatio
+			}
+			assert.Equal(t, float64(1), totalRatio)
+		})
+	}
+}
 
 func TestGetAreaInfo(t *testing.T) {
 	areaInfo, err := GetAreaInfo("JP")
@@ -12,7 +105,7 @@ func TestGetAreaInfo(t *testing.T) {
 	assert.Equal(t, "JP", areaInfo.Region)
 	assert.Equal(t, 47, len(areaInfo.Areas))
 	assert.Equal(t, Area{
-		Name: "北海道",
-		Size: 83424,
+		Name: "Hokkaido",
+		Size: float64(83424),
 	}, areaInfo.Areas[0])
 }
